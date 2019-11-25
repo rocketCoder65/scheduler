d7157c6bfa9172ccb4a279987ee3e2da97966597
Lucas Mateus Santos Pereira <rocketCoder65@gmail.com>
2019-11-24 23:18:00 -0300

    Fix config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@storybook/react';
-import requireContext from 'require-context.macro';
 
-const req = requireContext('../src/components', true, /\.stories\.js$/);
+const req = require.context('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
